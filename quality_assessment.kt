4fe950909180560ed1d72645a9a1184377bef924
Vlada <135242631+vladaa2a10aaaa4@users.noreply.github.com>
2026-01-07 13:43:24 +0300

    Update quality_assessment.kt

--- quality_assessment.kt
+++ quality_assessment.kt
@@ -4,16 +4,16 @@ fun input(prompt: String): String {
 }
 
 fun main() {
-    val rating = input("Оцените качество обслуживания от 1 до 5, где 1 - очень плохо, 5 - очень хорошо: ")
-    
+    val rating = input("Оцените качество обслуживания от 1 до 5, где 1 - очень плохо, 5 - очень хорошо: \n")
+
     val ratingNumber = rating.toIntOrNull()
     if (ratingNumber == null || ratingNumber !in 1..5) {
-        println("Ошибка: пожалуйста, введите число от 1 до 5")
+        println("Ошибка: пожалуйста, введите число от 1 до 5\n")
         return
     }
-    
-    val comment = input("Добавьте комментарий: ").takeIf { it.isNotBlank() } ?: "без комментария"
-    
+
+    val comment = input("Добавьте комментарий: \n").takeIf { it.isNotBlank() } ?: "без комментария"
+
     println("\n=== Результат ===")
     println("Ваша оценка: $ratingNumber")
     println("Ваш комментарий: $comment")
